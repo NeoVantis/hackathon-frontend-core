344e47b2e7116d5a945591c9628d901fd47e1186
Vaiditya2207 <vaidityatanwar2207@gmail.com>
2025-09-09 11:09:32 +0530

    correct build machanics

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+// Type declarations for CSS imports from external modules
+declare module '@neovantis/mantisui/styles' {
+  const content: string;
+  export default content;
+}
+
+// General CSS module type declarations
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
