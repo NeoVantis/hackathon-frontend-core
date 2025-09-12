c58170812545701c886fe97189cae30f9ec773a9
Agrapujya Lashkari <joebrion20@gmail.com>
2025-09-12 14:26:01 +0530

    added the Login SignUp button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import { Button} from '@neovantis/mantisui'
 import { useState } from 'react'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
-  const [loading, setLoading] = useState(false)
-
-  const clickLoading = () => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 4000)
-  }
 
   return (
-
-    <div style={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-    <Button variant="primary" icon="+" loading={loading} onClick={clickLoading}>
-        Add Content
-     </Button>
-    </div>
+    <>
+      <Signup/>
+      <Login/>
+    </>
   )
 }
 
