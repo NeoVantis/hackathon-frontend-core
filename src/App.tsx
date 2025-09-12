934fe4d7d45e8f4ec41c019e18da681ad611186f
Agrapujya Lashkari <joebrion20@gmail.com>
2025-09-12 15:10:56 +0530

    added the Email OTP Input component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Button} from '@neovantis/mantisui'
 import { useState } from 'react'
 import Login from './components/Login'
 import Signup from './components/Signup'
+import VerifyEmail from './components/Verifyemail'
 
 function App() {
 
   return (
     <>
-      <Signup/>
-      <Login/>
+      <VerifyEmail/>
     </>
   )
 }
