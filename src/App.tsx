6f4326c7f7ae9aa42e67c63120c79b99e2941f93
Agrapujya Lashkari <joebrion20@gmail.com>
2025-09-12 16:48:37 +0530

    fixed the build issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Button} from '@neovantis/mantisui'
-import { useState } from 'react'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import VerifyEmail from './components/Verifyemail'
-import VerifiedEmail from './components/Verifiedemail'
+// import { Button} from '@neovantis/mantisui'
+// import { useState } from 'react'
+// import Login from './components/Login'
+// import Signup from './components/Signup'
+// import VerifyEmail from './components/VerifyEmail'
+import VerifiedEmail from './components/VerifiedEmail'
 
 function App() {
 
